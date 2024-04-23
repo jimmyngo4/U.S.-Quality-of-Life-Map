fa22835961a7455dd6c58fd256f62dbe9cf5c78a
jimmyngo4 <105337446+jimmyngo4@users.noreply.github.com>
2024-04-23 12:59:12 -0400

    allow nullable

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,10 +3,18 @@ from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from models import Base, QualityOfLife
 
+import pandas as pd
 import csv
+from datetime import date
 
 from dotenv import load_dotenv
 import os
+
+
+def clean_data():
+    df = pd.read_csv('../city.csv')
+    df.replace('?', 'NULL', inplace=True)
+    df.to_csv('cleaned_data.csv')
 
 
 def load_data():
@@ -34,23 +42,25 @@ def load_data():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with open('../scraped_data.csv', 'r') as csvfile:
+    with open('cleaned_data.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader)  # skip header row
         for row in csvreader:
+            location = row[1].split(', ')
+
             new_row = QualityOfLife(
-                city=row[0],
-                state=row[1],
-                purchasing_power_index=float(row[2]),
-                safety_index=float(row[3]),
-                health_care_index=float(row[4]),
-                climate_index=float(row[5]),
-                cost_of_living_index=float(row[6]),
-                property_price_to_income_ratio=float(row[7]),
-                traffic_commute_time_index=float(row[8]),
-                pollution_index=float(row[9]),
-                quality_of_life_index=float(row[10]),
-                last_update=row[11]
+                city=location[0],
+                state=location[1],
+                purchasing_power_index=float(row[2]) if row[2] != 'NULL' else None,
+                safety_index=float(row[3]) if row[3] != 'NULL' else None,
+                health_care_index=float(row[4]) if row[4] != 'NULL' else None,
+                climate_index=float(row[5]) if row[5] != 'NULL' else None,
+                cost_of_living_index=float(row[6]) if row[6] != 'NULL' else None,
+                property_price_to_income_ratio=float(row[7]) if row[7] != 'NULL' else None,
+                traffic_commute_time_index=float(row[8]) if row[8] != 'NULL' else None,
+                pollution_index=float(row[9]) if row[9] != 'NULL' else None,
+                quality_of_life_index=float(row[10]) if row[10] != 'NULL' else None,
+                last_update=date.today()
             )
 
             session.merge(new_row)
@@ -60,4 +70,5 @@ def load_data():
 
 
 if __name__ == '__main__':
+    # clean_data()
     load_data()
